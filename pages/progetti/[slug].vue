7c2094d665747e97b8322e69afe456778ba3f121
schlatterca <48732275+schlatterca@users.noreply.github.com>
2024-08-11 13:23:36 +0200

    planimetria css

--- pages/progetti/[slug].vue
+++ pages/progetti/[slug].vue
@@ -34,7 +34,8 @@
                     <figure class="w-auto mx-20% bg-lightgrey mt-auto mb-auto">
                         <img v-if="myData.planimetria"
                         :src="imageUrlFor(myData.planimetria)"
-                        class="pic w-auto h-full m-auto object-cover mix-blend-multiply"/>
+                        class="pic h-full m-auto object-cover mix-blend-multiply
+                        !w-auto max-h-[80dvh]"/>
                     </figure>
                     <div class="m-auto w-[20rem] mx-auto DM-Mono leading-tight">
                         <p class="text-base uppercase" v-html="myData.title" v-if="myData.title"></p>
